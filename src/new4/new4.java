74985a8b1bc680dfa1872139d33ff4f1def23813
vikas.yadav24 <vikas.yadav24@MYSHEC596614L.ad.infosys.com>
2023-12-28 22:17:08 +0530

    third commit

---
+++ src/new4/new4.java
@@ -0,0 +1,25 @@
+package new4;
+import java.util.Scanner;
+public class new4 {
+
+	public static void main(String[] args) {
+		// TOimport java.util.Scanner;
+
+
+        Scanner scan = new Scanner(System.in);
+        int i = scan.nextInt();
+        double d = scan.nextDouble();
+        scan.nextLine();
+        String s = scan.nextLine();
+     
+        // Write your code here.  
+
+        System.out.println("String: " + s);
+        System.out.println("Double: " + d);
+        System.out.println("Int: " + i);
+    }
+
+
+	}
+
+
---
+++ src/new4/package-info.java
@@ -0,0 +1,7 @@
+package new4;
+
+public class Vikas{
+	public static void main(String[] args) {
+		System.out.print("vikas");
+	}
+}
